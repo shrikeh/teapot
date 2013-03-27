85696accc1a1e26bcefc00a71ef3be5efc16de75
Barney Hanlon <barney@shrikeh.net>
2013-03-27 10:27:59 +0000

    More psr compliance

--- lib/Teapot/HttpResponse/StatusCode.php
+++ lib/Teapot/HttpResponse/StatusCode.php
@@ -371,44 +371,63 @@ interface StatusCode
     const FORBIDDEN = 403;
 
     /**
-      * The server has not found anything matching the Request-URI. No indication is given of whether the condition is temporary or permanent. The 410 (Gone) status code
-      * SHOULD be used if the server knows, through some internally configurable mechanism, that an old resource is permanently unavailable and has no forwarding address.
-      * This status code is commonly used when the server does not wish to reveal exactly why the request has been refused, or when no other response is applicable.
+      * The server has not found anything matching the Request-URI. No
+      * indication is given of whether the condition is temporary or permanent.
+      * The 410 (Gone) status code SHOULD be used if the server knows, through
+      * some internally configurable mechanism, that an old resource is
+      * permanently unavailable and has no forwarding address.
+      * This status code is commonly used when the server does not wish to
+      * reveal exactly why the request has been refused, or when no other
+      * response is applicable.
       *
       * @var integer
       */
     const NOT_FOUND = 404;
 
     /**
-     * The method specified in the Request-Line is not allowed for the resource identified by the Request-URI. The response MUST include an Allow header containing a
-     * list of valid methods for the requested resource.
+     * The method specified in the Request-Line is not allowed for the resource
+     * identified by the Request-URI. The response MUST include an Allow header
+     * containing a list of valid methods for the requested resource.
      *
      * @var integer
      */
     const METHOD_NOT_ALLOWED = 405;
 
     /**
-      * The resource identified by the request is only capable of generating response entities which have content characteristics not acceptable according to the accept
-      * headers sent in the request.
+      * The resource identified by the request is only capable of generating
+      * response entities which have content characteristics not acceptable
+      * according to the accept headers sent in the request.
       *
-      * Unless it was a HEAD request, the response SHOULD include an entity containing a list of available entity characteristics and location(s) from which the user or
-      * user agent can choose the one most appropriate. The entity format is specified by the media type given in the Content-Type header field. Depending upon the
-      * format and the capabilities of the user agent, selection of the most appropriate choice MAY be performed automatically. However, this specification does not
-      * define any standard for such automatic selection.
-      *     Note: HTTP/1.1 servers are allowed to return responses which are not acceptable according to the accept headers sent in the request. In some cases, this
-      *     may even be preferable to sending a 406 response. User agents are encouraged to inspect the headers of an incoming response to determine if it is acceptable.
+      * Unless it was a HEAD request, the response SHOULD include an entity
+      * containing a list of available entity characteristics and location(s)
+      * from which the user or user agent can choose the one most appropriate.
+      * The entity format is specified by the media type given in the
+      * Content-Type header field. Depending upon the format and the
+      * capabilities of the user agent, selection of the most appropriate
+      * choice MAY be performed automatically. However, this specification does
+      * not define any standard for such automatic selection.
+      *     Note: HTTP/1.1 servers are allowed to return responses which are
+      *     not acceptable according to the accept headers sent in the request.
+      *     In some cases, this may even be preferable to sending a 406
+      *     response. User agents are encouraged to inspect the headers of an
+      *     incoming response to determine if it is acceptable.
       *
-      * If the response could be unacceptable, a user agent SHOULD temporarily stop receipt of more data and query the user for a decision on further actions.
+      * If the response could be unacceptable, a user agent SHOULD temporarily
+      * stop receipt of more data and query the user for a decision on further
+      * actions.
       *
       * @var integer
       */
     const NOT_ACCEPTABLE = 406;
 
     /**
-      * This code is similar to 401 (Unauthorized), but indicates that the client must first authenticate itself with the proxy. The proxy MUST return a
-      * Proxy-Authenticate header field (section 14.33) containing a challenge applicable to the proxy for the requested resource. The client MAY repeat the request
-      * with a suitable Proxy-Authorization header field (section 14.34). HTTP access authentication is explained in "HTTP Authentication: Basic and Digest Access
-      * Authentication" [43].
+      * This code is similar to 401 (Unauthorized), but indicates that the
+      * client must first authenticate itself with the proxy. The proxy MUST
+      * return a Proxy-Authenticate header field (section 14.33) containing a
+      * challenge applicable to the proxy for the requested resource. The
+      * client MAY repeat the request with a suitable Proxy-Authorization
+      * header field (section 14.34). HTTP access authentication is explained
+      * in "HTTP Authentication: Basic and Digest Access Authentication" [43].
       *
       * @var integer
       */
