df3a7a1808b785939ecf1de5194d663adb5effa5
shrikeh <barney@shrikeh.net>
2016-04-22 09:14:10 +0100

    Whitespace, need to create a git hook

--- src/StatusCode.php
+++ src/StatusCode.php
@@ -53,13 +53,13 @@ use Teapot\StatusCode\Http;
  */
 interface StatusCode extends Http
 {
-  const INFORMATIONAL = 1;
+    const INFORMATIONAL = 1;
 
-  const SUCCESSFUL = 2;
+    const SUCCESSFUL = 2;
 
-  const REDIRECTION = 3;
+    const REDIRECTION = 3;
 
-  const CLIENT_ERROR = 4;
+    const CLIENT_ERROR = 4;
 
-  const SERVER_ERROR = 5;
+    const SERVER_ERROR = 5;
 }
